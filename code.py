aa96d0e1741b31ea40a234a244f6328b16e6c728
Russell Eveleigh <russelleveleigh@gmail.com>
2023-02-02 07:20:36 +0000

    Binary functions added

--- code.py
+++ code.py
@@ -2,23 +2,16 @@ import time
 import board
 import neopixel
 
-
-# On CircuitPlayground Express, and boards with built in status NeoPixel -> board.NEOPIXEL
-# Otherwise choose an open pin connected to the Data In of the NeoPixel strip, i.e. board.D1
 pixel_pin = board.GP22
-
-# On a Raspberry pi, use this instead, not all pins are supported
-# pixel_pin = board.D18
-
-# The number of NeoPixels
 num_pixels = 16
-
-# The order of the pixel colors - RGB or GRB. Some NeoPixels have red and green reversed!
-# For RGBW NeoPixels, simply change the ORDER to RGBW or GRBW.
 ORDER = neopixel.GRB
 
 pixels = neopixel.NeoPixel(
-    pixel_pin, num_pixels, brightness=1, auto_write=False, pixel_order=ORDER
+    pixel_pin, 
+    num_pixels, 
+    brightness=1, 
+    auto_write=False, 
+    pixel_order=ORDER
 )
 
 
@@ -53,28 +46,110 @@ def rainbow_cycle(wait):
         time.sleep(wait)
 
 
-while True:
-    #Comment this line out if you have RGBW/GRBW NeoPixels
-    #pixels.fill((255, 0, 0))
-    # Uncomment this line if you have RGBW/GRBW NeoPixels
-    # pixels.fill((255, 0, 0, 0))
-    #pixels.show()
-    #time.sleep(1)
+def one(x):
+    for i in range(0,2):
+        if x == 1:
+            pixels[i] = (255, 255, 255)
+        else:
+            pixels[i] = (255, 0, 0)
+    
+def two(x):
+    for i in range(2,4):
+        if x == 1:
+            pixels[i] = (255, 255, 255)
+        else:
+            pixels[i] = (255, 0, 0)
 
-    # Comment this line out if you have RGBW/GRBW NeoPixels
-    #pixels.fill((0, 255, 0))
-    # Uncomment this line if you have RGBW/GRBW NeoPixels
-    # pixels.fill((0, 255, 0, 0))
-    #pixels.show()
-    #time.sleep(1)
+def four(x):
+    for i in range(4,6):
+        if x == 1:
+            pixels[i] = (255, 255, 255)
+        else:
+            pixels[i] = (255, 0, 0)
 
-    # Comment this line out if you have RGBW/GRBW NeoPixels
-    #pixels.fill((0, 0, 255))
-    # Uncomment this line if you have RGBW/GRBW NeoPixels
-    # pixels.fill((0, 0, 255, 0))
-    #pixels.show()
-    #time.sleep(1)
+def eight(x):
+    for i in range(6,8):
+        if x == 1:
+            pixels[i] = (255, 255, 255)
+        else:
+            pixels[i] = (255, 0, 0)
 
-    rainbow_cycle(0.001)  # rainbow cycle with 1ms delay per step
-    #wheel(255)
+def sixteen(x):
+    for i in range(8,10):
+        if x == 1:
+            pixels[i] = (255, 255, 255)
+        else:
+            pixels[i] = (255, 0, 0)
 
+def thirtytwo(x):
+    for i in range(10,12):
+        if x == 1:
+            pixels[i] = (255, 255, 255)
+        else:
+            pixels[i] = (255, 0, 0)
+
+def sixtyfour(x):
+    for i in range(12,14):
+        if x == 1:
+            pixels[i] = (255, 255, 255)
+        else:
+            pixels[i] = (255, 0, 0)
+
+def onetwoeight(x):
+    for i in range(14,16):
+        if x == 1:
+            pixels[i] = (255, 255, 255)
+        else:
+            pixels[i] = (255, 0, 0)
+
+
+def decToBinary(n):
+    y = n
+    if y >= 128:
+        onetwoeight(1)
+        y = y - 128
+    else:
+        onetwoeight(0)
+    if y >= 64:
+        sixtyfour(1)
+        y = y - 64
+    else:
+        sixtyfour(0)
+    if y >= 32:
+        thirtytwo(1)
+        y = y - 32
+    else:
+        thirtytwo(0)
+    if y >= 16:
+        sixteen(1)
+        y = y - 16
+    else:
+        sixteen(0)
+    if y >= 8:
+        eight(1)
+        y = y - 8
+    else:
+        eight(0)
+    if y >= 4:
+        four(1)
+        y = y - 4
+    else:
+        four(0)
+    if y >= 2:
+        two(1)
+        y = y - 2
+    else:
+        two(0)
+    if y >= 1:
+        one(1)
+        y = y - 1
+    else:
+        one(0)
+    pixels.show()
+
+count = 0
+
+while count < 256:
+    decToBinary(count)
+    time.sleep(1)
+    count = count + 1
